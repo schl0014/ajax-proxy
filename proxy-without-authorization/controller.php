d9a60731fd8a2eeb98660327508df0b98d572b62
schl0014 <schl0014@hz.nl>
2022-06-03 12:32:52 +0200

    working

--- proxy-without-authorization/controller.php
+++ proxy-without-authorization/controller.php
@@ -1,9 +1,9 @@
 <?php
 include("./proxy.php");
-
 //setup the Proxy with the right credentials
-$mySwapiProxy = new Proxy('https://swapi.dev/api');
+$mySwapiProxy = new Proxy('https://newsapi.org/v2/everything?q=max%20verstappen');
 //get the data
-$result = $mySwapiProxy->getData('people/1');
+$result = $mySwapiProxy->getData();
 //print the data to the frontend.
+
 print_r($result);
--- proxy-without-authorization/proxy.php
+++ proxy-without-authorization/proxy.php
@@ -1,36 +1,62 @@
 <?php
+
 /**
  * Class to handle an AJAX call as a proxy
  */
+
 class Proxy
+
 {
+
     private $url;
 
+
     public function __construct($url)
+
     {
+
         $this->url = $url;
+
     }
 
-    public function getData($collection)
+
+    public function getData()
+
     {
-        $request_url = $this->url . "/" . $collection . "/";
+
+        $request_url = $this->url;
+
         $curl = curl_init($request_url);
 
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-        ]);
-        
-        //API call with Authorization
-        //Change the key with your settings
-        // curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        //     'Content-Type: application/json',
-        //     'Authorization: Bearer key'
-        // ]);
 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+
+
+// curl_setopt($curl, CURLOPT_HTTPHEADER, [
+
+// 'Content-Type: application/json',
+
+// ]);
+
+//API call with Authorization
+
+//Change the key with your settings
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+
+            'Content-Type: application/json',
+
+            'Authorization: 886de0e5fdfc4c418e15410d188988dd'
+
+        ]);
         $response = curl_exec($curl);
+
         curl_close($curl);
 
+
         return $response;
+
     }
+
 }
